edea31e93446bb68b80245b2062939df9c308ced
VugarAhmadov <vuqar.ehmedov.94@gmail.com>
2024-12-30 01:36:02 +0400

    added remote 2 and remote 3

---
+++ apps/mfs/remote-2/rsbuild.config.ts
@@ -0,0 +1,37 @@
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+
+export default defineConfig({
+  html: {
+    template: "./src/index.html",
+  },
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: "remote2",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/app.tsx",
+      },
+      dts: false,
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  source: {
+    entry: {
+      index: "./src/index.tsx",
+    },
+    tsconfigPath: "./tsconfig.app.json",
+  },
+  server: {
+    port: 4202,
+  },
+  output: {
+    copy: [{ from: "./src/favicon.ico" }, { from: "./src/assets" }],
+    target: "web",
+    distPath: {
+      root: "dist",
+    },
+  },
+});
---
+++ apps/mfs/remote-2/src/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>REMOTE 2 MF</h1>
+    </div>
+  );
+}
